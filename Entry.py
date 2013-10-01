885c21ca4d98b2be15a2750ebb838c505ac088b6
Bennett Ng <bennett.k.ng@gmail.com>
2013-10-01 15:12:28 -0700

    clean up Entry.py

--- Entry.py
+++ Entry.py
@@ -8,18 +8,18 @@ class Entry():
 	
     name 					scientific name of the organism
 	description 			description of the organism
-    entry_type 				GENOME or VIRUS constant
-    entry_chromosome_count	number of chromoses in the organism's genome
-	entry_base_count		number of bases in the organism's genome
-    picture					URL of the picture to use for the cards
+    type 					GENOME or VIRUS constant
+    chromosome_count		number of chromoses in the organism's genome
+	base_count				number of bases in the organism's genome
+    picture_url				URL of the picture to use for the cards
     """
 
-    def __init__(self, name, description, entry_type, entry_chromosome_count, entry_base_count, picture_url):
+    def __init__(self, name, description, type, chromosome_count, base_count, picture_url):
 		self._name = name
 		self._description = description
-		self._entry_type = entry_type
-		self._entry_chromosome_count = entry_chromosome_count
-		self._entry_base_count = entry_base_count
+		self._type = type
+		self._chromosome_count = chromosome_count
+		self._base_count = base_count
 		self._picture_url = picture_url
 
     def get_name(self):
@@ -27,15 +27,15 @@ class Entry():
 		
 	def get_description(self):
 		return self._description
+	
+	def get_type(self):
+		return self._type
 
     def get_entry_chromosome_count(self):
         return self._chromosome_count
 		
 	def get_entry_base_count(self):
-        return self._base_count
-
-    def get_entry_value(self):
-        return self._entry_value
+		return self._base_count
 
     def get_picture_url(self):
         return self._picture_url
