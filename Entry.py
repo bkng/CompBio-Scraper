2144dec6f11d727e58c40181ac37f64bfcf90d02
Niraj Amalkanti <neji49@gmail.com>
2013-10-07 20:50:54 -0700

    Tests all work

--- Entry.py
+++ Entry.py
@@ -14,7 +14,7 @@ class Entry():
     def __init__(self, name, entry_type, entry_value, picture_url):
         self._name = name
         self._entry_type = entry_type
-        self._entry_value
+        self._entry_value = entry_value
         self._picture_url = picture_url
 
     def get_name(self):
@@ -27,4 +27,46 @@ class Entry():
         return self._entry_value
 
     def get_picture_url(self):
-        return self._pictuer_url
+        return self._picture_url
+
+class EntryBuilder():
+    """
+    Builder pattern class for Entry object.
+    """
+
+    def __init__(self):
+        """
+        Initializes all values to defaults.
+        """
+        self._name = ""
+        self._entry_type = GENOME
+        self._entry_value = 0
+        self._picture_url = ""
+
+    def set_name(self, name):
+        """
+        Sets name object.
+        """
+        self._name = name
+        
+    def set_entry_type(self, entry_type):
+        """
+        Sets entry type
+        """
+        self._entry_type = entry_type;
+
+    def set_entry_value(self, entry_value):
+        """
+        Sets entry value.
+        """
+        self._entry_value = entry_value
+
+    def set_picture_url(self, picture_url):
+        """
+        Sets picture url.
+        """
+        self._picture_url = picture_url
+
+    def build_entry(self):
+        return Entry(self._name, self._entry_type, 
+                self._entry_value, self._picture_url)
--- HTMLGenerator.py
+++ HTMLGenerator.py
@@ -1,5 +1,7 @@
+import Entry
+
 from lxml import etree
-from CompBioScraper.interface.Entry import Entry
+
 
 class HTMLGenerator:
     """
@@ -14,17 +16,25 @@ class HTMLGenerator:
         Returns html document for entry.
         """
         html_root = etree.Element("html")
+        css = etree.SubElement(html_root, "link")
+        css.attrib["rel"] = "stylesheet"
+        css.attrib["href"] = "../card.css"
         div_root = etree.SubElement(html_root, "div")
-        div_root.attrib["name"] = self._entry.get_name()
-        head = etree.SubElement(div_root, "head")
-        title = etree.SubElement(head, "title")
-        title.text = self._entry.get_name()
         body = etree.SubElement(div_root, "body")
+        title = etree.SubElement(body, "h1")
+        title.text = self._entry.get_name()
+        card_type_element = etree.SubElement(body, "h2")
+        card_type = self._entry.get_entry_type()
+        if card_type == Entry.VIRUS:
+            card_type_element.text = "VIRUS"
+        elif card_type == Entry.GENOME:
+            card_type_element.text = "GENOME"
         img = etree.SubElement(body, "img")
+        paragraph = etree.SubElement(body, "p")
         img.attrib["src"] = self._entry.get_picture_url()
-        if self._entry.get_entry_type() == VIRUS:
-            body.text = "Genome Modifier:{0}".format(self._entry.get_entry_value())
-        elif self._entry.get_entry_type() == GENOME:
-            body.text = "Genome:{0}".format(self._entry.get_entry_value())
+        if self._entry.get_entry_type() == Entry.VIRUS:
+            paragraph.text = "Genome Modifier:{0}".format(self._entry.get_entry_value())
+        elif self._entry.get_entry_type() == Entry.GENOME:
+            paragraph.text = "Genome:{0}".format(self._entry.get_entry_value())
         return html_root
 
